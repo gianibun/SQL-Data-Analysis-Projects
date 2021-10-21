0a140864512853182c11f7f564b5909063ebaf19
gianibun <92890530+gianibun@users.noreply.github.com>
2021-10-21 09:32:16 -0400

    Update WireData.sql
    Names have been changed to maintain confidentiality

--- WireData.sql
+++ WireData.sql
@@ -1,8 +1,12 @@
-SELECT EP.eclipse_id, EP.description, EP.buy_line_id, EP.price_line_id, EP.product_gl_type ,PIL.location,PIL.location_type, PIL.location_status, PIL.location_lot_id, PIL.location_qty, pil.branch_id
+/*
+My boss wants to get all of our wire inventory location information for later data analysis
+*/
+SELECT EP.eclipse_id, EP.description, EP.buy_line_id, EP.price_line_id, EP.product_gl_type ,PIL.location,
+       PIL.location_type, PIL.location_status, PIL.location_lot_id, PIL.location_qty, pil.branch_id
 FROM EDW_loebelectric_com_PROD.eclipse.product EP
-INNER JOIN EDW_loebelectric_com_PROD.eclipse.product_inventory_location PIL
-ON EP.eclipse_id = PIL.product_id
-WHERE price_line_id IN ('BWCU', 'MCCABLE', 'BWALUM')
+  INNER JOIN PROD.clipse.product_inventory_location PIL
+  ON EP.eclipse_id = PIL.product_id
+WHERE price_line_id IN ('BCU', 'MCABLE', 'BWLUM')
 
---'GENCBL', 'PORTCORD', 'SOUTHWRE', 'ENCREWRE','WIREMISC', 'HOUSTON', 'WHSESERV','OMNI','CWCC','SPECWIRE')
+
 
