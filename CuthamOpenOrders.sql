b0fd1a6d94845fb40244276b315f66989e8796ab
gianibun <92890530+gianibun@users.noreply.github.com>
2021-10-21 08:45:38 -0400

    Update CuthamOpenOrders.sql

--- CuthamOpenOrders.sql
+++ CuthamOpenOrders.sql
@@ -1,71 +1,30 @@
-/*Please provide ALL open orders for Eaton/Cutler hammer. Let’s have the following:
-•	PO #		
-•	Writer
+/*Please provide ALL open orders for Cutham. Letâ€™s have the following:
+â€¢	PO #		
+â€¢	Writer
 x	Internal notes
-•	Item ID
-•	Description
-•	Qty
-•	Cost
-•	Extended Cost
-•	Uom
-•	Order Date
-•	Ship date (promise date)
+â€¢	Item ID
+â€¢	Description
+â€¢	Qty
+â€¢	Cost
+â€¢	Extended Cost
+â€¢	Uom
+â€¢	Order Date
+â€¢	Ship date (promise date)
 */
-
-select *
-from EDW_loebelectric_com_PROD.eclipse.open_purchase_order -- PO#, status code, promised date/receive date, writer
-
-select *
-from EDW_loebelectric_com_PROD.eclipse.purchase_order_line_generation -- product id, gen id, line id,
-
-select *
-from EDW_loebelectric_com_PROD.eclipse.product -- Price line, Uom, COGS, desc
-
-select * 
-from EDW_loebelectric_com_PROD.eclipse.purchase_order_log -- create date/order date 
-order by purchase_order_id
-
---select *
---from EDW_loebelectric_com_PROD.eclipse.purchase_order_line
-
---select *
---from EDW_loebelectric_com_PROD.eclipse.purchase_order
-
---select * 
---from EDW_loebelectric_com_PROD.eclipse.purchase_order
-
---select *
---from EDW_loebelectric_com_PROD.eclipse.purchase_order_generation
-
-
-
 -----------
-select OPO.purchase_order_id, OPO.status_code, receive_date as promised_date, OPO.writer, POLG.product_id, stock_qty,receive_qty,price ,branch_cost_per_um, EP.description, ep.buy_line_id, POLG.generation_id, POLG.line_id, cogs_amount, total_amount
-from EDW_loebelectric_com_PROD.eclipse.open_purchase_order OPO
-
-left join EDW_loebelectric_com_PROD.eclipse.purchase_order_line_generation POLG
-on OPO.purchase_order_id = POLG.purchase_order_id and OPO.generation_id = POLG.generation_id
-
-left join EDW_loebelectric_com_PROD.eclipse.product EP 
-on POLG.product_id = EP.product_id
-
-where buy_line_id like 'CUTHAM' --AND POLG.purchase_order_id like 'P100097992'
-
---Merge with create date
---NULL create date = copied from another PO 
--- Final Query, run this query 
-
+--Merge 4 different tables to get all of the above data-- 
+-----------
 select OPO.purchase_order_id, OPO.status_code, receive_date as promised_date, POL.create_date, OPO.writer,ep.buy_line_id, POLG.product_id, EP.description, stock_qty,receive_qty,price ,branch_cost_per_um,  POLG.generation_id, POLG.line_id, cogs_amount, total_amount
-from EDW_loebelectric_com_PROD.eclipse.open_purchase_order OPO
+from PROD.clipse.open_purchase_order OPO
 
-left join EDW_loebelectric_com_PROD.eclipse.purchase_order_line_generation POLG
+left join PROD.clipse.purchase_order_line_generation POLG
 on OPO.purchase_order_id = POLG.purchase_order_id and OPO.generation_id = POLG.generation_id
 
-left join EDW_loebelectric_com_PROD.eclipse.product EP 
+left join PROD.clipse.product EP 
 on POLG.product_id = EP.product_id
 
-left join EDW_loebelectric_com_PROD.eclipse.purchase_order_log POL
+left join PROD.clipse.purchase_order_log POL
 on POL. purchase_order_id = OPO.purchase_order_id
 
-where buy_line_id like 'CUTHAM' and status_code in ('O','V') --and POL.create_date < '8/1/21'--AND POLG.purchase_order_id like 'P100076598'
+where buy_line_id like 'CUTHAM' and status_code in ('O','V') 
 order by 1
